7582c310c097f86b838e54e4babe56ceb0564958
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-08 19:45:19 -0400

    Final push

--- creatingInstances-Records.sh
+++ creatingInstances-Records.sh
@@ -2,10 +2,11 @@
 
 image=ami-03265a0778a880afb
 security_grp=sg-07486e05b46c9943d
-subnet="subnet-0dd54bd754c61211a"
+subnet=subnet-0dd54bd754c61211a
 services=("mongodb" "mysql" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 Instancetype=""
-
+domainname=saikiransudhireddy.com
+hostedzone=Z05570443S15P72O43ST7
 for i in "${services[@]}"
 do
     if [[ $i=="mongodb" || $i=="mysql" ]];
@@ -17,17 +18,17 @@ do
     IP_address=$(aws ec2 run-instances --image-id $image  --instance-type $Instancetype --security-group-ids $security_grp --subnet-id $subnet --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r ".Instances[0].PrivateIpAddress")
     echo "created the instance with private IP : $IP_address"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z05570443S15P72O43ST7 --change-batch '{
+    aws route53 change-resource-record-sets --hosted-zone-id $hostedzone --change-batch '{
         "Changes": [
             {
                 "Action": "CREATE",
                 "ResourceRecordSet": {
-                    "Name": "$i.$domainname",
+                    "Name": "'$i.$domainname'",
                     "Type": "A",
                     "TTL": 300,
                     "ResourceRecords": [
                         {
-                            "Value": "$IP_address"
+                            "Value": "'$IP_address'"
                         }
                     ]
                 }
