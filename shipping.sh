a73365b769be756d1d08b2f00f83b8954bb9a74f
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-07 22:04:53 -0400

    redis modified

--- shipping.sh
+++ shipping.sh
@@ -1,103 +1,189 @@
+# #!/bin/bash
+# id=$(id -u)
+# R="\e[31m"
+# N="\e[0m"
+# Y="\e[33m"
+# date=$(date +%F-%H-%M-%S)
+# script_name=$0
+# #logfiles=/tmp/shell-script-logs/$script_name-$date.log 
+
+# if [ $id -ne 0 ]
+# then
+#     echo -e "$R ERROR: $N you do not have the sudo access, please install with root access"
+#     exit 1
+# fi
+
+# validate(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$R  $2  is FAiled $N"
+#         exit 1
+#     else 
+#         echo -e "$Y $2 is success"
+#     fi
+# }
+
+# test -d /tmp/shell-script-logs
+# if [ $? -ne 0 ]
+# then
+#     mkdir /tmp/shell-script-logs 
+#     validate $? "setup an shell-script-log directory"
+# fi
+# logfiles=/tmp/shell-script-logs/$script_name-$date.log
+
+# yum install maven -y  &>>$logfiles
+
+# validate $? "installing maven"
+
+# id roboshop &>>$logfiles
+
+# if [ $? -ne 0 ]
+# then
+#     useradd roboshop &>>$logfiles
+#     validate $? "Adding application User"
+# fi
+
+# test -d /app
+# if [ $? -ne 0 ]
+# then
+#     mkdir /app &>>$logfiles
+#     validate $? "setup an app directory"
+# fi
+
+# curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>>$logfiles
+
+# validate $? "importing shipping files"
+
+# cd /app &>>$logfiles
+
+
+
+# unzip -o /tmp/shipping.zip &>>$logfiles
+
+# validate $? "unzipping the shipping"
+
+# cd /app &>>$logfiles
+
+# mvn clean package &>>$logfiles
+
+# validate $? "installing dependencies"
+
+# mv target/shipping-1.0.jar shipping.jar &>>$logfiles
+
+# validate $? "moving shipping.jar"
+
+# cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>$logfiles
+
+# validate $? "copying the configuration"
+
+# systemctl daemon-reload &>>$logfiles
+
+# validate $? "reloading the configuration"
+
+
+
+# systemctl enable shipping  &>>$logfiles
+
+# validate $? "enabling shipping"
+
+# systemctl restart shipping &>>$logfiles
+
+# validate $? "starting shipping"
+
+# yum install mysql -y &>>$logfiles
+
+# validate $? "installing mysql"
+
+# mysql -u -h mysql.saikiransudhireddy.com -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>$logfiles
+
+# validate $? "loading the schema"
+
+# systemctl restart shipping &>>$logfiles
+
+# validate $? "restarting the shipping"
+
 #!/bin/bash
-id=$(id -u)
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
 R="\e[31m"
+G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-date=$(date +%F-%H-%M-%S)
-script_name=$0
-#logfiles=/tmp/shell-script-logs/$script_name-$date.log 
 
-if [ $id -ne 0 ]
+if [ $USERID -ne 0 ];
 then
-    echo -e "$R ERROR: $N you do not have the sudo access, please install with root access"
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
 
-validate(){
-    if [ $1 -ne 0 ]
+VALIDATE(){
+    if [ $1 -ne 0 ];
     then
-        echo -e "$R  $2  is FAiled $N"
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
-    else 
-        echo -e "$Y $2 is success"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-test -d /tmp/shell-script-logs
-if [ $? -ne 0 ]
-then
-    mkdir /tmp/shell-script-logs 
-    validate $? "setup an shell-script-log directory"
-fi
-logfiles=/tmp/shell-script-logs/$script_name-$date.log
+yum install maven -y &>>$LOGFILE
 
-yum install maven -y  &>>$logfiles
+VALIDATE $? "Installing Maven"
 
-validate $? "installing maven"
+useradd roboshop &>>$LOGFILE
 
-id roboshop &>>$logfiles
+mkdir /app &>>$LOGFILE
 
-if [ $? -ne 0 ]
-then
-    useradd roboshop &>>$logfiles
-    validate $? "Adding application User"
-fi
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>>$LOGFILE
 
-test -d /app
-if [ $? -ne 0 ]
-then
-    mkdir /app &>>$logfiles
-    validate $? "setup an app directory"
-fi
+VALIDATE $? "Downloading shipping artifact"
 
-curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>>$logfiles
+cd /app &>>$LOGFILE
 
-validate $? "importing shipping files"
+VALIDATE $? "Moving to app directory"
+ 
+unzip /tmp/shipping.zip &>>$LOGFILE
 
-cd /app &>>$logfiles
+VALIDATE $? "Unzipping shipping"
 
+mvn clean package &>>$LOGFILE
 
+VALIDATE $? "packaging shipping app"
 
-unzip -o /tmp/shipping.zip &>>$logfiles
+mv target/shipping-1.0.jar shipping.jar &>>$LOGFILE
 
-validate $? "unzipping the shipping"
+VALIDATE $? "renaming shipping jar"
 
-cd /app &>>$logfiles
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>$LOGFILE
 
-mvn clean package &>>$logfiles
+VALIDATE $? "copying shipping service"
 
-validate $? "installing dependencies"
+systemctl daemon-reload &>>$LOGFILE
 
-mv target/shipping-1.0.jar shipping.jar &>>$logfiles
+VALIDATE $? "daemon-reload"
 
-validate $? "moving shipping.jar"
+systemctl enable shipping  &>>$LOGFILE
 
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>$logfiles
+VALIDATE $? "Enabling shipping"
 
-validate $? "copying the configuration"
+systemctl start shipping &>>$LOGFILE
 
-systemctl daemon-reload &>>$logfiles
-
-validate $? "reloading the configuration"
+VALIDATE $? "Starting shipping"
 
 
+yum install mysql -y  &>>$LOGFILE
 
-systemctl enable shipping  &>>$logfiles
+VALIDATE $? "Installing MySQL client"
 
-validate $? "enabling shipping"
+mysql -h mysql.saikiransudhireddy.com -uroot -pRoboShop@1 < /app/schema/shipping.sql  &>>$LOGFILE
 
-systemctl restart shipping &>>$logfiles
+VALIDATE $? "Loaded countries and cities info"
 
-validate $? "starting shipping"
+systemctl restart shipping &>>$LOGFILE
 
-yum install mysql -y &>>$logfiles
-
-validate $? "installing mysql"
-
-mysql -u -h mysql.saikiransudhireddy.com -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>$logfiles
-
-validate $? "loading the schema"
-
-systemctl restart shipping &>>$logfiles
-
-validate $? "restarting the shipping"
+VALIDATE $? "Restarting shipping"
