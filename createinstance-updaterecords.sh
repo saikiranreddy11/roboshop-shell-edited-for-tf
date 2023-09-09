52c75447d5f53a21b93cc77fce2bd302d174bc45
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-08 20:41:53 -0400

    Final push

--- createinstance-updaterecords.sh
+++ createinstance-updaterecords.sh
@@ -36,36 +36,77 @@ do
     #     ]
     # }'
 
-    aws route53 change-resource-record-sets --hosted-zone-id $hostedzone --change-batch '{
-        "Changes": [
-            {
-                "Action": "UPSERT",
-                "ResourceRecordSet": {
-                    "Name": "'$i.$domainname'",
-                    "Type": "A",
-                    "TTL": 300,
-                    "ResourceRecords": [
-                        {
-                            "Value": "'$IP_address'"
-                        }
-                    ]
+   #!/bin/bash
+
+# Define your variables
+# EXISTING_DOMAIN_NAME="example.com"    # Name of the existing DNS record
+# NEW_DOMAIN_NAME="new.example.com"     # Name of the new DNS record
+# NEW_IP_ADDRESS="123.456.789.123"     # New IP address to set
+
+# Check if the existing DNS record exists
+aws route53 list-resource-record-sets \
+    --hosted-zone-id $hostedzone \
+    --query "ResourceRecordSets[?Name == '$i.$domainname']" > /dev/null
+
+# $? will contain the exit code of the previous command
+if [ $? -eq 0 ]; then
+    # The existing DNS record exists, update it
+    echo "Updating existing DNS record..."
+    aws route53 change-resource-record-sets \
+        --hosted-zone-id $hostedzone \
+        --change-batch '{
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": "'$i.$domainname.'",
+                        "Type": "A",
+                        "TTL": 300,
+                        "ResourceRecords": [
+                            {
+                                "Value": "'$IP_address'"
+                            }
+                        ]
+                    }
                 }
-            },
-            {
-                "Action": "CREATE",
-                "ResourceRecordSet": {
-                    "Name": "'$i.$domainname'",
-                    "Type": "A",
-                    "TTL": 300,
-                    "ResourceRecords": [
-                        {
-                            "Value": "'$IP_address'"
-                        }
-                    ]
+            ]
+        }'
+else
+    # The existing DNS record does not exist, create it
+    echo "Creating a new DNS record..."
+    aws route53 change-resource-record-sets \
+        --hosted-zone-id <YOUR_HOSTED_ZONE_ID> \
+        --change-batch '{
+            "Changes": [
+                {
+                    "Action": "CREATE",
+                    "ResourceRecordSet": {
+                        "Name": "'$i.$domainname.'",
+                        "Type": "A",
+                        "TTL": 300,
+                        "ResourceRecords": [
+                            {
+                                "Value": "'$IP_address'"
+                            }
+                        ]
+                    }
                 }
-            }
-        ]
-    }'
+            ]
+        }'
+fi
+
+# # Check if the EC2 instance exists, and create it if it doesn't
+# INSTANCE_ID=$(aws ec2 describe-instances \
+#     --filters "Name=tag:Name,Values=my-instance" \
+#     --query "Reservations[0].Instances[0].InstanceId" --output text 2>/dev/null)
+
+# if [ -z "$INSTANCE_ID" ]; then
+#     echo "Creating a new EC2 instance..."
+#     # Replace with your EC2 instance creation command here
+#     # Example: aws ec2 run-instances ...
+# else
+#     echo "EC2 instance already exists."
+# fi
 
 
 done
